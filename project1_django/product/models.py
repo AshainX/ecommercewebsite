1aa64ebc755d40d45c49a6655cd16494d96504ae
Ashutosh Swain <79051730+AshainX@users.noreply.github.com>
2022-11-06 23:44:27 +0530

    support commits

--- project1_django/product/models.py
+++ project1_django/product/models.py
@@ -1,15 +1,66 @@
+from io import BytesIO
+from PIL import Image
+
+from django.core.files import File
 from django.db import models
 
-class Category(models.Models):
-    name = models.CharField(max_length=255) #name of the category
-    slug = models.SlugField() #address version of the name
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
 
-    class Meta:     #order the categories by the name
-        ordering = ('name',) #tuple ('name',)
-
-    def __str__(self):    # string representation of the category what type of  object it is
+    class Meta:
+        ordering = ('name',)
+    
+    def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
 
-    def get_absolute_url(self): #get the url of the categiry easier to use in frontend later
-        return f'/{self.slug}/' #address of the category
-     
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_added',)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/{self.category.slug}/{self.slug}/'
+    
+    def get_image(self):
+        if self.image:
+            return 'http://127.0.0.1:8000' + self.image.url
+        return ''
+    
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return 'http://127.0.0.1:8000' + self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+
+                return 'http://127.0.0.1:8000' + self.thumbnail.url
+            else:
+                return ''
+    
+    def make_thumbnail(self, image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
